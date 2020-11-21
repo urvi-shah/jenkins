427e6af72cfc45271afe1ff4eeae68c59d003c65
urvi-shah <umshah99@gmail.com>
2020-11-21 13:20:10 +0530

    jenkins integration SCM

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ class HelloWorld
 
 public static void main(String [] args)
 {
-System.out.println("Hello World! This is Urvi");
+System.out.println("Hi! I am performing Git and GitHub Integration with Jenkins SCM ");
 }
 
 }
